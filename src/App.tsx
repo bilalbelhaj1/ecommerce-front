7dbaae45b5b85c14a9a93dd09c93616fbae88161
bilalbelhaj1 <bilalbelhadj2025@gmail.com>
2026-04-10 19:56:12 +0100

    Setup tailwind and shadcn UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
+import { Button } from "./components/ui/button"
 function App() {
-  return <h1 className='text-amber-300'>HEllo</h1>
+  return <>
+    <h1 className='text-amber-300'>HEllo</h1>
+    <Button>Hello</Button>
+  </>
 }
 
 export default App
