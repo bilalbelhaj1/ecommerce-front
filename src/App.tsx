8fd5ffcbe967b959d31fec7f434cbab982b5ca5b
bilalbelhaj1 <bilalbelhadj2025@gmail.com>
2026-04-20 18:00:55 +0100

    setup react-router define routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,20 @@
 import Layout from "./components/Layout";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Orders from "./pages/admin/Orders";
+
 const App = () => {
-  return <Layout/>
-}
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<h1>Hello</h1>}/>
+        <Route path="/admin" element={<Layout />}>
+          <Route index element={<div>Dashboard</div>} />
+          <Route path="orders" element={<Orders />} />
+        </Route>
+
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App;
---
+++ src/pages/admin/Orders.tsx
@@ -0,0 +1,8 @@
+
+const Orders = () => {
+    return (
+        <div>Dashboard</div>
+    )
+}
+
+export default Orders;
